9228b52d7405e7456c2ae592f63b47a0180dc3b0
Rodrigo Luca <102487398+Noctec8@users.noreply.github.com>
2024-03-25 09:05:42 -0300

    Atualização Porjto
    Atualização Projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import './App.css';
 import { categories } from './api';
-import Row from './Comandos/Row';
-
+import Row from './Comandos/Row'
 
 function App() {
   return (
     <div>
-      {/* Navbar */}
-      {/* Banner */}
-      {/* Categorias - Linhas */}
-      {/* <Navbar/>
-      <Banner/> */}
-      {categories.map( (category) => {
-        return <Row 
-                  key={category.name}
-                  title={category.title}
-                  isLarge={category.isLarge}
-                  path={category.path}
-                />
-      })}
+
+        {/*Navbar*/}
+        {/*Banner*/}
+        {/*Categorias - Linhas*/}
+
+        {categories.map( (category) => {
+
+            return  <Row 
+                         key={category.name}
+                         title={category.title} 
+                         isLarge={category.isLarge}
+                         path={category.path}
+                    />
+
+        })}
+
     </div>
   );
 }
--- src/Comandos/Row.js
+++ src/Comandos/Row.js
@@ -1,29 +1,47 @@
-import { useEffect, useState } from 'react';
-import { getData } from '../api'
-import './Row.css';
+import { useEffect } from 'react'
+import './Row.css'
+import {getData} from '../api'
+import { useState } from 'react'
 
-export default function Row (props){
-    const [filmes, setFilmes] = useState()
-    let image_host = 'https://image.tmdb.org/t/p/original/'
+export default function Row({title, path, isLarge}) {
+    const[filmes, setFilmes] = useState()
+    let imageHost = 'https://image.tmdb.org/t/p/original/'
 
     let fetchData = async () => {
-        let response = await getData(props.path)
-        return response
+
+        let result = await getData(path)
+
+        return result
     }
 
-    useEffect( () => {
-        fetchData().then((data)=>{
+    useEffect( ()=>{
+
+        let consulta = fetchData()
+        
+        consulta.then((data)=>{
+            console.log(data?.results)
             setFilmes(data?.results)
         })
-    },[])
 
-    return(
-        <div>
-            {props.title}
-            <div className='images'>
-                {filmes?.map((filme) => {
-                    let width = props.isLarge? 'large':'normal'
-                    return <img className={width} key={filme?.id} src={image_host + filme.backdrop_path}></img> 
+
+    }, [])
+
+    return (
+        <div className='row-container'>
+
+            <h2 className='row-header'>{title}</h2>
+
+            <div className='row-cards'>
+                {filmes?.map( (filme) => {
+                    
+                    return (
+                        <img
+                        className={`movie-card ${isLarge && "movie-card-large"}` }
+                        key={filme.id} 
+                        src={imageHost + (isLarge?filme.backdrop_path:filme.poster_path)} 
+                        alt={filme.name} >
+                        </img>
+                    )                
                 })}
             </div>
         </div>
